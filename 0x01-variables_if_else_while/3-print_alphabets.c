bd4a2b5f9ddf39251b6ad1438c12facfda6021d2
Nuhamin <dnuhamin7@gmail.com>
2022-12-12 09:54:33 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,13 +11,19 @@
 
 int main(void)
 {
-	char low;
+	int ch = 'a';
+	int CH = 'A';
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-		putchar(low);
-		putchar('\n');
-
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	
+	putchar('\n');
 	return (0);
 }
