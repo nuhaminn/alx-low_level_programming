0343be570dd03e99109e3076d2e3e96a35db2ed7
Nuhamin <dnuhamin7@gmail.com>
2022-12-13 11:30:54 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add -> adds two integers and returns the result
+ * @a: parameter1
+ * @b: parameter2
+ * return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
