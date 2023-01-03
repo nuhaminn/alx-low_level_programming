5b3cc9586db15818a15e365db3b8385b47a55318
Nuhamin <dnuhamin7@gmail.com>
2023-01-03 11:40:01 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp -> for comparing purpose
+ * @s1: para1
+ * @s2: para2
+ * Return: Always 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+	result = strcmp(s1, s2);
+
+	return (result);
+}
