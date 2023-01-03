9b779141d346097fd47d6e15f44587bfb73da2c3
Nuhamin <dnuhamin7@gmail.com>
2023-01-03 08:44:05 -0800

    8-print_array.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,10 +16,10 @@ void rev_string(char *s)
 		counter++;
 
 	for (i = 0; i < counter; i++)
-			{
-				counter--;
-				rev = s[i];
-				s[i] = s[counter];
-				s[counter] = rev;
-			}
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
 }
