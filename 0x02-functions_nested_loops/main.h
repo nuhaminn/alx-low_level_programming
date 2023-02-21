e08b2a7843d3d203d107aed2108feb6ac8298f0f
Nuhamin <dnuhamin7@gmail.com>
2023-02-21 08:16:16 -0800

    Functions nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifdef main_h
-	#define main_h
+#ifdef MAIN_H
+#define MAIN_H
 
 int _putchar(char);
 void print_alphabet(void);
@@ -14,5 +14,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
 #endif
